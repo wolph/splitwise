fcf5a9983c340a4518a156981061e855cfaf3e11
Rick van Hattem <Wolph@wol.ph>
2014-04-02 14:47:16 +0200

    moved static and templates dirs

--- splitwise/application.py
+++ splitwise/application.py
@@ -4,19 +4,19 @@ import flask_failsafe
 @flask_failsafe.failsafe
 def get_app():
     import flask
-    import flask_bootstrap
     from flask.ext import compress
 
     # Create the app
-    app = flask.Flask(__name__)
+    app = flask.Flask(
+        __name__.split('.')[0],
+        static_folder='../static',
+        template_folder='../templates',
+    )
 
     # Load the configs
     app.config.from_object('splitwise.default_settings')
     app.config.from_pyfile('../settings.py', silent=True)
     app.config.from_envvar('SPLITWISE_SETTINGS', silent=True)
-
-    # Enable the bootstrap layout
-    flask_bootstrap.Bootstrap(app)
 
     # Enable javascript/css compression
     compress.Compress(app)
